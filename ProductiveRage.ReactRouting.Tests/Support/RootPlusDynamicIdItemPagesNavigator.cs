83c65c820013d3f00ccaa37da521426e9f6555fb
ProductiveRage <dangger36@gmail.com>
2016-05-09 19:16:55 +0100

    Added further unit tests

---
+++ ProductiveRage.ReactRouting.Tests/Support/RootPlusDynamicIdItemPagesNavigator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Bridge.React;
+using ProductiveRage.Immutable;
+using ProductiveRage.ReactRouting.Tests.Support.Actions;
+
+namespace ProductiveRage.ReactRouting.Tests.Support
+{
+	public sealed class RootPlusDynamicIdItemPagesNavigator<T> : Navigator
+	{
+		private readonly Func<UrlDetails> _getRoot;
+		private readonly Func<NonBlankTrimmedString, UrlDetails> _getItem;
+		public RootPlusDynamicIdItemPagesNavigator(Set<NonBlankTrimmedString> parentSegments, IInteractWithBrowserRouting historyHandler, AppDispatcher dispatcher)
+			: base(parentSegments, historyHandler, dispatcher)
+		{
+			if (dispatcher == null)
+				throw new ArgumentNullException("dispatcher");
+
+			AddRelativeRoute(
+				Set<string>.Empty,
+				new NavigateToRoot<T>()
+			);
+			_getRoot = () => GetPath();
+
+			AddRelativeRoute(
+				RouteBuilder.Empty.Fixed("item").String(),
+				matchedValue => new NavigateToItem<T>(matchedValue.Item1)
+			);
+			_getItem = segment => GetPath("item", segment.Value);
+		}
+		public RootPlusDynamicIdItemPagesNavigator(IInteractWithBrowserRouting historyHandler, AppDispatcher dispatcher)
+			: this(Set<NonBlankTrimmedString>.Empty, historyHandler, dispatcher) { }
+
+		public UrlDetails Root() { return _getRoot(); }
+		public UrlDetails Item(NonBlankTrimmedString name) { return _getItem(name); }
+	}
+}
---
+++ ProductiveRage.ReactRouting.Tests/Support/RootPlusHotelAndProductSectionsNavigator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Bridge.React;
+using ProductiveRage.Immutable;
+using ProductiveRage.ReactRouting.Tests.Support.Actions;
+using ProductiveRage.ReactRouting.Tests.Support.RouteDataTypes;
+
+namespace ProductiveRage.ReactRouting.Tests.Support
+{
+	public sealed class RootPlusHotelAndRestaurantSectionsNavigator : Navigator
+	{
+		private readonly Func<UrlDetails> _getRoot;
+		public RootPlusHotelAndRestaurantSectionsNavigator(IInteractWithBrowserRouting historyHandler, AppDispatcher dispatcher)
+			: base(Set<NonBlankTrimmedString>.Empty, historyHandler, dispatcher)
+		{
+			if (dispatcher == null)
+				throw new ArgumentNullException("dispatcher");
+
+			AddRelativeRoute(
+				Set<string>.Empty,
+				new NavigateToRoot()
+			);
+			_getRoot = () => GetPath();
+
+			Hotels = new RootPlusDynamicIdItemPagesNavigator<Hotel>(
+				Set.Of(new NonBlankTrimmedString("hotel")),
+				historyHandler,
+				dispatcher
+			);
+			foreach (var route in Hotels.Routes)
+				AddRelativeRoute(route);
+
+			Restaurants = new RootPlusDynamicIdItemPagesNavigator<Restaurant>(
+				Set.Of(new NonBlankTrimmedString("restaurant")),
+				historyHandler,
+				dispatcher
+			);
+			foreach (var route in Restaurants.Routes)
+				AddRelativeRoute(route);
+		}
+
+		public UrlDetails Root() { return _getRoot(); }
+		public RootPlusDynamicIdItemPagesNavigator<Hotel> Hotels { get; private set; }
+		public RootPlusDynamicIdItemPagesNavigator<Restaurant> Restaurants { get; private set; }
+	}
+}
