d623a42579862967a6f9d1800a38a92c93b98984
ProductiveRage <dangger36@gmail.com>
2016-05-09 20:55:28 +0100

    Filled out some TODO summary comments

--- ProductiveRage.ReactRouting/OptionalNonBlankTrimmedStringExtensions.cs
+++ ProductiveRage.ReactRouting/OptionalNonBlankTrimmedStringExtensions.cs
@@ -6,7 +6,10 @@ namespace ProductiveRage.ReactRouting
 	public static class OptionalNonBlankTrimmedStringExtensions // TODO: Move into ProductiveRage.Immutable.Extensions NuGet packages
 	{
 		/// <summary>
-		/// TODO
+		/// Translate an Optional NonBlankTrimmedString instance into a string - returning null if there is no value. Nulls are usually not desirable since it's
+		/// difficult for the type system to describe where a null is and isn't acceptable (which is what the Optional struct is intended to help with) but, if
+		/// an Optional NonBlankTrimmedString is to be used as a class name of an element then it will need to be reduced to a string instance again at some
+		/// point since React elements have a string ClassName property (which may be null, meaning set no class attribute).
 		/// </summary>
 		public static string ToNullableString(this Optional<NonBlankTrimmedString> source)
 		{
@@ -14,7 +17,8 @@ namespace ProductiveRage.ReactRouting
 		}
 
 		/// <summary>
-		/// TODO
+		/// Combine two Optional NonBlankTrimmedString instances if both arguments have values, separating them with a single space. If only one argument has a
+		/// value then that argument will be returned. If neither argument have a value then a missing value will be returned.
 		/// </summary>
 		public static Optional<NonBlankTrimmedString> Add(this Optional<NonBlankTrimmedString> source, Optional<NonBlankTrimmedString> other)
 		{
@@ -22,7 +26,9 @@ namespace ProductiveRage.ReactRouting
 		}
 
 		/// <summary>
-		/// TODO
+		/// Combine two Optional NonBlankTrimmedString instances if both arguments have values, separating them with a specified delimiter string (which may be
+		/// blank if desired). If only one argument has a value then that argument will be returned. If neither argument have a value then a missing value will
+		/// be returned.
 		/// </summary>
 		public static Optional<NonBlankTrimmedString> Add(this Optional<NonBlankTrimmedString> source, string delimiter, Optional<NonBlankTrimmedString> other)
 		{
