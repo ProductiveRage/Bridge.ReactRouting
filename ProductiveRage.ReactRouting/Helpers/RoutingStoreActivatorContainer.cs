a6d4c0b2390c4d28da36c2e3f9fc33c00a319996
ProductiveRage <dangger36@gmail.com>
2017-01-25 10:48:53 +0000

    Changed RoutingStoreActivatorContainer to re-dispatch navigation messages to workaround async SetState behaviour
    This behaviour is the default but may be disabled by passing false for
    the RoutingStoreActivatorContainer's
    redispatchNavigationMessagesAfterStateAsynchronouslyUpdated constructor
    argument. Because SetState is (sometimes) processed asynchronously, if
    navigation actions are used to change the currently-mounted container
    component AND by stores to initiate data retrievals and if the data
    retrieval completes before the container is mounted and subscribes to
    the store's Change event then the component may miss the Change event
    fired by the store and so may not realise that data is available when it
    renders. Re-issueing navigation messages after the (potentially async)
    SetState work has been completed means that the store can fire its
    Change event again AFTER the container is known to have mounted, which
    means that the container will definitely receive the event.

--- ProductiveRage.ReactRouting/Helpers/RoutingStoreActivatorContainer.cs
+++ ProductiveRage.ReactRouting/Helpers/RoutingStoreActivatorContainer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Bridge.Html5;
 using Bridge.React;
 using ProductiveRage.Immutable;
 
@@ -11,12 +12,12 @@ namespace ProductiveRage.ReactRouting.Helpers
 	/// </summary>
 	public sealed class RoutingStoreActivatorContainer : Component<RoutingStoreActivatorContainer.Props, RoutingStoreActivatorContainer.State>
 	{
-		public RoutingStoreActivatorContainer(IDispatcher dispatcher, NavigateActionMatcher navigateActionMatcher)
-			: base(new Props(dispatcher, navigateActionMatcher)) { }
+		public RoutingStoreActivatorContainer(IDispatcher dispatcher, NavigateActionMatcher navigateActionMatcher, bool redispatchNavigationMessagesAfterStateAsynchronouslyUpdated = true)
+			: base(new RoutingStoreActivatorContainer.Props(dispatcher, navigateActionMatcher, redispatchNavigationMessagesAfterStateAsynchronouslyUpdated)) { }
 
 		protected override State GetInitialState()
 		{
-			return new State(currentContainerRetriever: null);
+			return State.Empty;
 		}
 
 		protected override void ComponentWillMount()
@@ -29,33 +30,81 @@ namespace ProductiveRage.ReactRouting.Helpers
 
 				var newContainerRetriever = props.NavigateActionMatcher.MatchAction(navigationAction);
 				if (newContainerRetriever.IsDefined)
-					SetState(state.With(_ => _.CurrentContainerRetriever, newContainerRetriever.Value));
+				{
+					var container = newContainerRetriever.Value();
+					var newState = state.WithCurrentContainer(container);
+					if (newState == state)
+						return; // Do nothing if there is no change to the current container component
+
+					SetState(
+						newState,
+						() =>
+						{
+							// When a navigation action occurs that results in the current container being changed, we may have a problem due to React's async state-updating
+							// nature. When container components are mounted/unmounted they will subscribe/unsubscribe to change events in relevant stores - if one of the stores
+							// receives this navigation action and uses it to retrieve some initial data and then fires its Change event, the container component that would be
+							// responsible for receiving that Change event and rendering the data may not currently be listening because it may have been unmounted, even though
+							// the navigation action that will result in its mounting has been dispatched (because the component-mounting is performed asynchronously and may be
+							// delayed sufficiently that the store has done its data retrieval before the container subscribes to its Change event). One way to workaround this
+							// is to allow this component to re-issue the navigation message after the container has been updated - doing that here means that there is only
+							// one place that hack is required. One alternative would be for the container component to initiate work after it has been mounted; the container
+							// could issue a subsequent dispatcher message (which would also be a hack because one action - the navigation action in this case - should not
+							// directly result in another dispatcher message being issued). Alternatively, the container could have a way to query the store and request that
+							// it update its data to the latest (which would be hack because the stores should instruct the containers when data has changed, rather than
+							// containers poking stores to update their content). So it seems like the least of all evils is to re-issue the navigation message here (which
+							// is also not ideal because it means that the handling of one dispatcher message is resulting in another dispatch-message call and it might
+							// seem odd to anyone watching the queue; "why are these messages appearing twice?") - it's optional, though, so if the consumer disagrees
+							// then they can set redispatchNavigationMessagesAfterStateAsynchronouslyUpdated to false.
+							if (props.RedispatchNavigationMessagesAfterStateAsynchronouslyUpdated)
+							{
+								// In some cases, SetState will be performed SYNCHRONOUSLY and so we need to re-dispatch the message using a SetTimeout call to ensure that
+								// the Dispatcher doesn't realise that the handler for a message is also dispatching a message
+								Window.SetTimeout(() =>
+								{
+									if (message.Source == MessageSourceOptions.Server)
+										props.Dispatcher.HandleServerAction(message.Action);
+									else
+										props.Dispatcher.HandleViewAction(message.Action);
+								});
+							}
+						}
+					);
+				}
 			});
 		}
 
 		public override ReactElement Render()
 		{
-			return state.CurrentContainerRetriever.IsDefined ? state.CurrentContainerRetriever.Value() : null;
+			return state.CurrentContainerRetrieverIfAny;
 		}
-
 		public sealed class Props : IAmImmutable
 		{
-			public Props(IDispatcher dispatcher, NavigateActionMatcher navigateActionMatcher)
+			public Props(IDispatcher dispatcher, NavigateActionMatcher navigateActionMatcher, bool redispatchNavigationMessagesAfterStateAsynchronouslyUpdated)
 			{
 				this.CtorSet(_ => _.Dispatcher, dispatcher);
 				this.CtorSet(_ => _.NavigateActionMatcher, navigateActionMatcher);
+				this.CtorSet(_ => _.RedispatchNavigationMessagesAfterStateAsynchronouslyUpdated, redispatchNavigationMessagesAfterStateAsynchronouslyUpdated);
 			}
-			public IDispatcher Dispatcher { get; private set; }
-			public NavigateActionMatcher NavigateActionMatcher { get; private set; }
+			public IDispatcher Dispatcher { get; }
+			public NavigateActionMatcher NavigateActionMatcher { get; }
+			public bool RedispatchNavigationMessagesAfterStateAsynchronouslyUpdated { get; }
 		}
 
-		public sealed class State : IAmImmutable
+		// Note: Can't use Optional<ReactElement> because ReactElement is [External], which means that it can't be used with Optional<>
+		public sealed class State
 		{
-			public State(Optional<Func<ReactElement>> currentContainerRetriever)
+			public static State Empty { get; } = new State(null);
+			private State(ReactElement currentContainerRetrieverIfAny)
 			{
-				this.CtorSet(_ => _.CurrentContainerRetriever, currentContainerRetriever);
+				CurrentContainerRetrieverIfAny = currentContainerRetrieverIfAny;
 			}
-			public Optional<Func<ReactElement>> CurrentContainerRetriever { get; private set; }
+			public ReactElement CurrentContainerRetrieverIfAny { get; }
+			public State WithCurrentContainer(ReactElement container)
+			{
+				if (container == null)
+					throw new ArgumentNullException(nameof(container));
+				return (container == CurrentContainerRetrieverIfAny) ? this : new State(container);
+			}
 		}
 	}
 }
