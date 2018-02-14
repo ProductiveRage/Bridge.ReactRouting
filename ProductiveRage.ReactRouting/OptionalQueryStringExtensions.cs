6388ed219d12c6114ee877bad86582309f48357d
ProductiveRage <dangger36@gmail.com>
2018-02-14 14:19:16 +0000

    Added some extension methods to make it easier to build and deconstruct QueryStrings

---
+++ ProductiveRage.ReactRouting/OptionalQueryStringExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ProductiveRage.Immutable;
+
+namespace ProductiveRage.ReactRouting
+{
+	public static class OptionalQueryStringExtensions
+	{
+		/// <summary>
+		/// Try to get an int value from the QueryString (if it has a value) by taking the first QueryString value for the specified key (if it exists in the QueryString) and parsing as an int - if the
+		/// QueryString is not defined or if it does not contain any values for the specified key or if the first value is not parseable as an int then a Missing value will be returned
+		/// </summary>
+		public static Optional<int> Int(this Optional<QueryString> source, string key)
+		{
+			var rawValue = String(source, key);
+			if (!rawValue.IsDefined)
+				return Optional<int>.Missing;
+
+			int value;
+			return int.TryParse(rawValue.Value, out value) ? value : Optional<int>.Missing;
+		}
+
+		/// <summary>
+		/// Try to get a single string value from the QueryString (if it has a value) by taking the first QueryString value for the specified key (if it exists in the QueryString) - if the QueryString
+		/// is not defined or if it does not contain any values for the specified key then a Missing value will be returned
+		/// </summary>
+		public static Optional<NonBlankTrimmedString> String(this Optional<QueryString> source, string key)
+		{
+			if (string.IsNullOrWhiteSpace(key))
+				throw new ArgumentException($"Null/blank {nameof(key)} specified");
+
+			if (!source.IsDefined)
+				return Optional<NonBlankTrimmedString>.Missing;
+
+			var rawValues = source.Value[key];
+			if (rawValues.Count == 0)
+				return Optional<NonBlankTrimmedString>.Missing;
+			var rawValue = rawValues[0];
+			return !rawValue.IsDefined || string.IsNullOrWhiteSpace(rawValue.Value) ? Optional<NonBlankTrimmedString>.Missing : new NonBlankTrimmedString(rawValue.Value);
+		}
+	}
+}
---
+++ ProductiveRage.ReactRouting/UrlDetailsExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ProductiveRage.Immutable;
+
+namespace ProductiveRage.ReactRouting
+{
+	public static class UrlDetailsExtensions
+	{
+		/// <summary>
+		/// Add a new QueryString segment to a UrlDetails instance. The value will have ToString called on it to generate the QueryString segment value. This will throw an exception for a null source, a
+		/// null-or-blank-or-whitespace-only key or a null value.
+		/// </summary>
+		public static UrlDetails AddToQuery(this UrlDetails source, string key, object value)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+			if (string.IsNullOrWhiteSpace(key))
+				throw new ArgumentException($"Null/blank {nameof(key)} specified");
+			if (value == null)
+				throw new ArgumentNullException(nameof(value));
+
+			return new UrlDetails(
+				source.Segments,
+				source.QueryString.GetValueOrDefault(new QueryString(NonNullList<QueryString.Segment>.Empty)).Add(key, value.ToString())
+			);
+		}
+
+		/// <summary>
+		/// Add a new QueryString segment to a UrlDetails instance if the specified value is defined (if a Missing value is provided then the source UrlDetails instance will be returned unaltered).
+		/// The value will have ToString called on it to generate the QueryString segment value. This will throw an exception for a null source or null-or-blank-or-whitespace-only key.
+		/// </summary>
+		public static UrlDetails AddToQueryIfDefined<T>(this UrlDetails source, string key, Optional<T> value)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+			if (string.IsNullOrWhiteSpace(key))
+				throw new ArgumentException($"Null/blank {nameof(key)} specified");
+
+			return value.IsDefined ? AddToQuery(source, key, value.Value) : source;
+		}
+	}
+}
