e095801e49f0b9d60c2b56cb6dacd0bf7880b60d
ProductiveRage <dangger36@gmail.com>
2018-02-14 10:08:14 +0000

    Fix merge issue

--- ProductiveRage.ReactRouting/Link.cs
+++ ProductiveRage.ReactRouting/Link.cs
@@ -101,7 +101,7 @@ namespace ProductiveRage.ReactRouting
 						e.PreventDefault();
 					}
 				},
-				props.Content
+				props.Text
 			);
 		}
 
@@ -120,7 +120,7 @@ namespace ProductiveRage.ReactRouting
 				Optional<IInteractWithBrowserRouting> historyHandlerOverride)
 			{
 				this.CtorSet(_ => _.Url, url);
-				this.CtorSet(_ => _.Content, content);
+				this.CtorSet(_ => _.Text, text);
 				this.CtorSet(_ => _.CaseSensitiveUrlMatching, caseSensitiveUrlMatching);
 				this.CtorSet(_ => _.Name, name);
 				this.CtorSet(_ => _.Target, target);
