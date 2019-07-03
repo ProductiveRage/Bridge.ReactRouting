1fd7432a3300c8f215356ef672248d16f771862e
Dan Roberts <dangger36@gmail.com>
2019-07-03 19:26:23 +0100

    (7.1.0) Rebuild against Bridge 17.9.0

--- ProductiveRage.ReactRouting/Properties/AssemblyInfo.cs
+++ ProductiveRage.ReactRouting/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ProductiveRage.ReactRouting")]
-[assembly: AssemblyCopyright("Copyright © ProductiveRage 2018")]
+[assembly: AssemblyCopyright("Copyright © ProductiveRage 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.0.0")]
+[assembly: AssemblyVersion("7.1.0.0")]
+[assembly: AssemblyFileVersion("7.1.0.0")]
